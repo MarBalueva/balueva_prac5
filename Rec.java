f3d6ecfeb0e6ba8a295c7da3d653e6c68a96b165
Maria Balueva <90175945+MarBalueva@users.noreply.github.com>
2021-09-27 11:55:32 +0300

    Add files via upload

--- Rec.java
+++ Rec.java
@@ -19,24 +19,14 @@ public class Rec {
         }
     }
 
-    public static int sum_of_k(int length, int sum, int k, int s){ //упражнение 4
-        if (length == k) //если длина совпала с нужным количеством знаков
-            if (sum == s) //если сумма совпала с нужной
-                return 1;
-            else
-                return 0;
-        int c;
-        if (length == 0) //фиксируем первую цифру числа (не может начинаться с нуля)
-            c = 1;
+     public static int num_of_digit(int x, int kol){ //упражнение 4
+        if (x == 0)
+            return kol;
         else
-            c = 0;
-        int res = 0; //количество чисел
-        for (int i = c; i < 10; i++) //перебираем числа от 1-значных до k-значных
-            res += sum_of_k(length + 1, sum + i, k, s);
-        return res;
+            return num_of_digit(x / 10, ++kol);
     }
 
-    public static int sum(int n, int s){ //упражнение 5
+    public static int sum(int n, int s){ //упражнение 4-5
         if (n == 0) //условие выхода из рекурсии
             return s;
         else
@@ -58,10 +48,18 @@ public class Rec {
 
         //упражнение 4
         System.out.println("Exercise 4");
-        System.out.println("Input k and s: ");
+        System.out.println("Input k and d: ");
         int k1 = in.nextInt(); //количество знаков в числе
-        int s1 = in.nextInt(); //сумма цифр
-        System.out.println("There are " + sum_of_k(0,0, k1, s1) + " " + k1 +"-digit numbers with the sum = " + s1); //примечание: аргумент s не может превышать значения 9 * k
+        int d = in.nextInt(); //сумма цифр
+        int s[] = new int[10]; //массив чисел
+        System.out.println("Input 10 numbers: ");
+        int res = 0; //количество чисел
+        for (int i = 0; i < 10; i++) {
+            s[i] = in.nextInt(); //заполнение массива
+            if ((num_of_digit(Math.abs(s[i]), 0) == k1) && (sum(Math.abs(s[i]),0) == d)) //используем рекурсивные методы для проверки суммы цифр числа и количества знаков
+                res++;
+        }
+        System.out.println("There are " + res + " " + k1 +"-digit numbers with the sum = " + d); //примечание: аргумент s не может превышать значения 9 * k
 
         //упражнение 5
         System.out.println("Exercise 5");
